d0fe36048d7340df003fd1ddebd5b8012dbc93d8
NAKANO Ryusuke <rsk.nakano@gmail.com>
2013-12-31 16:47:49 +0900

    fix viewer paging algorithm
    * fix number of image-box divs: (size of images) -> 3.
    ** To avoid crash caused by many translate3d divs.

--- public/js/init.js
+++ public/js/init.js
@@ -1,54 +1,57 @@
-var imageBoxList;
 var currentBoxIndex = 0;
 
-var loadEmptyImageBoxes = function(){
-  for(var i = 0; i < imageFileList.length; i++){
-    var imageBox = $("<div>").addClass("image-box").append($("<img>").attr("src", "").addClass("image"));
-    $("#swipe-box").append(imageBox);
-  }
-};
-
 var loadInitialImages = function(){
-  loadImage(0);
-  loadImage(1);
+  loadImage(0, CENTER);
+  loadImage(1, LEFT);
 };
 
-var loadImage = function(i){
-  $(".image-box img")[i].src = imageFileList[i];
+var createEmptyImageBox = function(i, url){
+  return $("<div>").addClass("image-box").append($("<img>").attr("src", url).addClass("image")).attr("id", "image-box-" + i);
+};
+
+var LEFT = 1, RIGHT = -1, CENTER = 0;
+
+var loadImage = function(i, direction){
+  var width = $(window).width();
+  var imageBox = createEmptyImageBox(i, imageFileList[i]);
+  if(direction == LEFT) {
+    Suzuka.Animation.moveX(imageBox, width + 50);
+  } else if(direction == RIGHT) {
+    Suzuka.Animation.moveX(imageBox, -1 * width - 50);
+  } else if(direction == CENTER) {
+    Suzuka.Animation.moveX(imageBox, 0);
+  }
+  $("#swipe-box").append(imageBox);
   console.log("load: " + i);
 };
 
 var unloadImage = function(i){
-  $(".image-box img")[i].src = "";
+  searchImageBox(i).remove();
   console.log("unload: " + i);
 };
 
-var setupImageBox = function(){
-    imageBoxList = $(".image-box");
-    var width = $(window).width();
-    
-    for(var i = 0; i < imageBoxList.length; i++){
-      Suzuka.Animation.moveX($(imageBoxList[i]), width * i);
-      $(imageBoxList[i]).show();
-    }
+var searchImageBox = function(i){
+  return $("#image-box-" + i);  
+};
 
+var setupImageBox = function(){
     Suzuka.Swipe.detector( $("#swipe-box"), {
       onLeftSwipe : function(){
-        if(currentBoxIndex < imageBoxList.length - 1) {
-          Suzuka.Animation.leftSwipe($(imageBoxList[currentBoxIndex]),
-                                     $(imageBoxList[currentBoxIndex+1]));
+        if(currentBoxIndex < imageFileList.length - 1) {
+          Suzuka.Animation.leftSwipe(searchImageBox(currentBoxIndex),
+                                     searchImageBox(currentBoxIndex+1));
           if(currentBoxIndex > 0) unloadImage(currentBoxIndex-1);
-          loadImage(currentBoxIndex+2);
+          loadImage(currentBoxIndex+2, LEFT);
           currentBoxIndex += 1;
         }
       },
       onRightSwipe : function(){
         if(currentBoxIndex > 0) {
-          Suzuka.Animation.rightSwipe($(imageBoxList[currentBoxIndex]),
-                                      $(imageBoxList[currentBoxIndex-1]));
-          if(currentBoxIndex < imageBoxList.length - 1)
+          Suzuka.Animation.rightSwipe(searchImageBox(currentBoxIndex),
+                                      searchImageBox(currentBoxIndex-1));
+          if(currentBoxIndex < imageFileList.length - 1)
             unloadImage(currentBoxIndex+1);
-          loadImage(currentBoxIndex - 2);
+          loadImage(currentBoxIndex - 2, RIGHT);
           currentBoxIndex -= 1;
         }
       }
@@ -56,7 +59,6 @@ var setupImageBox = function(){
   };
 
 $(function(){
-  loadEmptyImageBoxes();
   loadInitialImages();
   setupImageBox();
 });
